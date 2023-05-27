cb8271a10fe8614aa50730d45f5cc3774aa0a492
Kyle Li <kyleseanli@gmail.com>
2023-05-27 18:15:29 -0400

    Removed leading / from static.

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # Here, they well be accessible at your-domain.onrender.com/static/...
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 # Following settings only make sense on production and may break development environments.
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
