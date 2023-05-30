3e9017bbb618d4bb7bf841981bcdd2b24b6fc503
Kyle Li <kyleseanli@gmail.com>
2023-05-29 21:22:34 -0400

    added static_root else case

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,7 +134,9 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
