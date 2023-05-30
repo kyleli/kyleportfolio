efb6848238ce8598dd28f36ba5ac04b7ad965d98
Kyle Li <kyleseanli@gmail.com>
2023-05-29 23:20:03 -0400

    added custom domain

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 DEBUG = 'RENDER' not in os.environ
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'kylejli.com',
+]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
